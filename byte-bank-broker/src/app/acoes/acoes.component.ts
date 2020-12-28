dedeea4149fbcbae306d2dfe060438444315c521
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-12-28 20:31:10 -0300

    extraindo informacoes de uma API

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,28 +1,17 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
-import { Acoes } from './modelo/acoes';
 import { AcoesService } from './acoes.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-acoes',
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent implements OnInit, OnDestroy {
+export class AcoesComponent {
   acoesInput = new FormControl();
-  acoes: Acoes;
-  private subscription: Subscription;
+  acoes$ = this.acoesService.getAcoes();
 
   constructor(private acoesService: AcoesService) { }
 
-  ngOnInit() {
-    this.subscription = this.acoesService.getAcoes()
-      .subscribe(acoes => this.acoes = acoes);
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
 }
